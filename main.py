05f3d020ed713f49f1865f3a3a05c6e6f9b5071d
XuanShine <xuan.ng@hotmail.com>
2019-12-05 23:00:29 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,138 @@
+import pickle
+import time
+from pprint import pprint
+from datetime import datetime, timedelta
+import xmlrpc.client
+
+from price import price_for_double_eco
+
+url = "https://wired.wubook.net/xrws/"
+server = xmlrpc.client.ServerProxy(url, verbose=False)
+
+
+# LOAD ID
+# File where identification is:
+with open("../id_pywubook.pkl", "rb") as f_in:
+    info = pickle.load(f_in)
+user = info["user"]
+password = info["password"]
+pkey = info["pkey"]
+lcode = info["lcode"]
+
+res, token = server.acquire_token(user, password, pkey)
+
+type_room = {"329039": "double economic",
+             "329667": "double balcony",
+             "329670": "triple economic",
+             "405126": "single balcony",
+             "405127": "single economic",
+             "407751": "triple balcony"
+            }
+
+def get_avail(dfrom, dto):
+    """Get avail from dfrom to dto in the wubook server
+    RETURN {<date>: {
+                     <code_chambre>:<disponibilité>, ...}
+            ...}"""
+    
+    return_code, avail = server.fetch_rooms_values(token, lcode, dfrom, dto)
+    if return_code != 0:
+        raise ConnectionError(avail)
+
+    dfrom_time = datetime.strptime(dfrom, "%d/%m/%Y")
+    dto_time = datetime.strptime(dto, "%d/%m/%Y")
+    days_diff = (dto_time - dfrom_time).days
+
+    result = dict()
+    for i in range(days_diff):
+        temp_dict = dict()
+        for room in type_room:
+            temp_dict[room] = avail[room][i].get("avail", 0)
+        result[(dfrom_time + timedelta(days=i)).strftime("%d/%m/%Y")] = temp_dict
+    return result
+
+
+def sum_avail(avail, list_code=("329039", "329667", "329670")):
+    """Total des disponibilités par jour des chambres dans list_code.
+    INPUT: {<date>: {<code_chambre>: <disponibilité, ...},
+            ... }
+        voir fonction get_avail(dfrom, dto)
+    RETURN {<date>: <total_disponibilité>, ...}"""
+    result = dict()
+    for date, avail_day in avail.items():
+        total_avail_day = 0
+        for room, avail in avail_day.items():
+            if room in list_code:
+                total_avail_day += avail
+        result[date] = total_avail_day
+    return result
+
+
+def update_price(room_code, date_price):
+    """Update new price in the wubook server
+    If existing price in wubook "triple eco" ends with .99, then doesn’t modify price.
+    INPUT date_price: {<date>: <price>, ..}
+    To update, we need to call:
+        update_plan_prices(token, lcode, pid, dfrom, prices)
+        where prices = { <room_code> : [price1, price2, ...], ...}
+    In this function, prices = [price1, price2, ...]
+    
+    RETURN dates and prices that was not changed {<date>: <price>, ..} or None """
+
+    date_price_tuple = list(sorted(date_price.items(), key=lambda dico: datetime.strptime(dico[0], "%d/%m/%Y")))
+    days = len(date_price_tuple) - 1  # On exclu le dernier jour.
+    dfrom = date_price_tuple[0][0]
+    dto = (datetime.strptime(dfrom, "%d/%m/%Y") + timedelta(days=days)).strftime("%d/%m/%Y")
+    prices = [price for _, price in date_price_tuple]
+
+    if date_price_tuple[-1][0] != dto:
+        raise Exception("Les dates ne sont pas continues pour mettre à jour les prix. Il doit manquer des dates: " + str(date_price_tuple))
+
+    # Prevent price modification if triple eco endswith .99
+    _, plan_prices = server.fetch_plan_prices(token, lcode, 0, dfrom, dto)
+    triple_eco_prices = plan_prices["329670"]
+    ignore = dict()  # to be return
+    for i in range(len(triple_eco_prices)):
+        if str(triple_eco_prices[i]).endswith(".99"):
+            prices[i] = plan_prices[room_code][i]
+            date_tmp = (dfrom.strptime("%d/%m/%Y") + timedelta(days=i)).strftime("%d/%m/%Y")
+            ignore[date_tmp] = prices[i]
+    
+    # Call wubook function for update
+    server.update_plan_prices(token, lcode, 0, dfrom, {room_code: prices})
+    
+    return ignore
+
+
+def update_price_automatic(period=60):
+    """update price in the next period (in days)
+    and print ignore dates"""
+    dfrom_time = datetime.today()
+    dfrom = dfrom_time.strftime("%d/%m/%Y")
+    dto_time = dfrom_time + timedelta(days=period)
+    dto = dto_time.strftime("%d/%m/%Y")
+    avail = get_avail(dfrom, dto)
+    total_avail = sum_avail(avail)
+
+    price_double_eco = dict()
+    price_double_balcon = dict()
+    for date, avail in total_avail.items():
+        price_double_eco[date] = round(price_for_double_eco(avail), 2)  # Détermine les nouvelles valeurs
+        price_double_balcon[date] = round(price_double_eco[date] * 1.1, 2)  # Balcon à 10% plus élevé.
+    ignore = dict()
+    ignore["double_eco"] = update_price("329039", price_double_eco)  # deco
+    ignore["double_balcon"] = update_price("329667", price_double_balcon)  # dblc
+    ignore["triple"] = update_price("329670", price_double_eco)  # prix triple fixée sur deco
+
+    pprint(ignore)
+    
+
+def main():
+    update_price_automatic()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        server.release_token(token)
---
+++ price.py
@@ -0,0 +1,70 @@
+import sys
+
+increase = 1  # in %
+base_price = 47
+min_price = 44
+max_price = 100
+n_rooms = 30
+n_room_min_price = 3
+n_room_increase = 1
+
+def graph_price():
+    """Show a list of evolution of price"""
+    prices = [base_price]
+    for i in range(n_rooms // n_room_increase + 1):
+        prices.append(prices[-1] * (1+increase*0.01))
+    print(prices)
+
+def price_for_double_eco(total_avail):
+    """ Return the suggest price for a double eco according to total_avail """
+    if total_avail > n_rooms - n_room_min_price:
+        return min_price
+    elif total_avail <= 0:
+        return max_price
+
+    prices = [base_price]
+    for i in range(n_rooms // n_room_increase + 1):
+        prices.append(prices[-1] * (1 + increase/100))
+    
+    i = (n_rooms - n_room_min_price - total_avail) // n_room_increase  # indice de l’augmentation
+
+    return prices[i]
+
+def test_price_for_double_eco():
+    """For
+        increase = 8  # in %
+        base_price = 44
+        min_price = 39
+        max_price = 100
+        n_rooms = 30
+        n_room_min_price = 2
+    """
+
+    assert price_for_double_eco(30) == 39
+    assert price_for_double_eco(31) == 39
+    assert price_for_double_eco(29) == 39
+
+    assert price_for_double_eco(28) == 44
+    assert price_for_double_eco(26) == 44
+
+    assert price_for_double_eco(25) == 47.52
+    assert price_for_double_eco(23) == 47.52
+
+    assert price_for_double_eco(22) - 51.31 <= 0.1
+    assert price_for_double_eco(20) - 51.31 <= 0.1
+
+    assert price_for_double_eco(3) - 81.43 <= 0.1
+    assert price_for_double_eco(2) - 81.43 <= 0.1
+
+    assert price_for_double_eco(1) - 87.95 <= 0.1
+
+    assert price_for_double_eco(0) == 100
+    assert price_for_double_eco(-1) == 100
+
+    
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "graph":
+            graph_price()
+        else:
+            print(price_for_double_eco(int(sys.argv[1])))
