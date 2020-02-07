2facbdcd80b4adb5b1532be7cb75160780fc592c
XuanShine <xuan.ng@hotmail.com>
2020-02-07 10:59:50 +0100

    Utilisation de "Rate" pour les différents tarifs

--- price.py
+++ price.py
@@ -1,4 +1,6 @@
 import sys
+import math
+from dataclasses import dataclass
 
 increase = 5  # in %
 base_price = 49
@@ -16,98 +18,126 @@ special_dates = {  # augmentation en % : 50% => 50
     "03/03/2020" : 10,  # Événement inconnu
 
     "07/03/2020" : 10,  # duathlon Grasse
-
+    # Fête du jasmin
+    # Fête de la rose
+    # Concours de danse
+    # ASA GRASSE
+    # VTT dans Grasse
 
 }
 
-def calcul_price(date, price_init):
-    return price_init
+@dataclass
+class Rate:
+    n_rooms: int
+    n_room_increase: int  # Augmente au bout de <n_room_increase> chambres réservées
+    min_price: float
+    # Les deux variables suivants sont dépendants l’un de l’autre.
+    increase: int  # (in %)
+    max_price: float
 
 
-def graph_price():
+low_season = Rate(
+    n_rooms=25,
+    n_room_increase=4,
+    min_price=49,
+    increase=None,
+    max_price=69
+)
+# mid_season = Rate()
+# full_season = Rate()
+# july = Rate()
+# congres = Rate()
+# saturday = Rate()  # mariage
+# christmas_new_year = Rate()
+
+
+def calcul_price(total_avail: int, date: str=None, rate: Rate=None, add_percent: float=0):
+    rate = rate or low_season  # if not rate
+
+    if rate.max_price:
+        n_rate = rate.n_rooms // rate.n_room_increase
+        if rate.n_rooms % rate.n_room_increase == 0:  # évite les cas où le nombre de chambre peut être divisé par le nombre de chambre à augmenter ce qui faussent les résultats
+           n_rate -= 1
+        rate.increase = (math.pow(rate.max_price/rate.min_price, 1 / n_rate) - 1) * 100
+    
+    if rate.increase:
+            prices = [rate.min_price]
+            for i in range(rate.n_rooms // rate.n_room_increase):
+                prices.append(prices[-1] * (1 + rate.increase/100))
+            
+            i = (rate.n_rooms - total_avail) // rate.n_room_increase  # indice de l’augmentation
+
+    res = prices[i] * (1 + add_percent/100)
+    assert res >= 40
+    return math.floor(res)
+
+
+def graph_price(rate):
     """Show a list of evolution of price"""
-    prices = [base_price]
-    for _ in range(n_rooms // n_room_increase + 1):
-        prices.append(round(prices[-1] * (1+increase*0.01)))
+    if rate.max_price:
+        n_rate = rate.n_rooms // rate.n_room_increase
+        if rate.n_rooms % rate.n_room_increase == 0:  # évite les cas où le nombre de chambre peut être divisé par le nombre de chambre à augmenter ce qui faussent les résultats
+           n_rate -= 1
+        rate.increase = (math.pow(rate.max_price/rate.min_price, 1 / n_rate) - 1) * 100
+    
+    if rate.increase:
+            prices = [rate.min_price]
+            for i in range(rate.n_rooms // rate.n_room_increase):
+                prices.append(prices[-1] * (1 + rate.increase/100))
     print(prices)
 
 def price_for_double_eco(total_avail, date=None):
     """ Return the suggest price for a double eco according to total_avail 
     can be also according to <date>: dd/mm/yyyy"""
-    if total_avail > n_rooms - n_room_min_price:
-        return min_price
-    elif total_avail <= 0:
-        return max_price
+    return calcul_price(total_avail=total_avail, date=date, rate=low_season)
 
-    prices = [base_price]
-    for i in range(n_rooms // n_room_increase + 1):
-        prices.append(prices[-1] * (1 + increase/100))
-    
-    i = (n_rooms - n_room_min_price - total_avail) // n_room_increase  # indice de l’augmentation
-    
-    if date:
-        if date in special_dates:
-            calculated_price = prices[i] * (1 + special_dates[date]/100)
-        else:
-            calculated_price = calcul_price(date, prices[i])
-    else:
-        calculated_price = prices[i]
 
-    assert calculated_price >= min_price
-    return calculated_price
+def test_calcul_price_increase_set():
+    rate1 = Rate(n_rooms=11, n_room_increase=3, min_price=40, increase=5, max_price=None)
+    rate2 = Rate(n_rooms=12, n_room_increase=4, min_price=40, increase=5, max_price=None)
 
-def test_price_for_double_eco():
-    """For
-    # TODO Tests need to be fix
-        increase = 8  # in %
-        base_price = 44
-        min_price = 39
-        max_price = 100
-        n_rooms = 30
-        n_room_min_price = 2
-        n_room_increase = 3
-    """
-    global increase
-    global base_price
-    global min_price
-    global max_price
-    global n_room
-    global n_room_min_price
-    global n_room_increase
+    assert calcul_price(11, None, rate1, 0) == 40
+    assert calcul_price(10, None, rate1, 3) == 41
+    assert calcul_price(9, None, rate1, 0) == 40
+    assert calcul_price(8, None, rate1, 0) == 42
+    assert calcul_price(6, None, rate1, 0) == 42
+    assert calcul_price(2, None, rate1, 0) == 46
+    assert calcul_price(1, None, rate1, 0) == 46
 
-    increase = 8  # in %
-    base_price = 44
-    min_price = 39
-    max_price = 100
-    n_rooms = 30
-    n_room_min_price = 2
-    n_room_increase = 3
 
-    assert price_for_double_eco(30) == 39
-    assert price_for_double_eco(31) == 39
-    assert price_for_double_eco(29) == 39
+    assert calcul_price(12, None, rate2, 0) == 40
+    assert calcul_price(10, None, rate2, 0) == 40
+    assert calcul_price(9, None, rate2, 0) == 40
+    assert calcul_price(8, None, rate2, 0) == 42
+    assert calcul_price(5, None, rate2, 0) == 42
+    assert calcul_price(4, None, rate2, 0) == 44
+    assert calcul_price(1, None, rate2, 0) == 44
 
-    assert price_for_double_eco(28) == 44
-    assert price_for_double_eco(26) == 44
 
-    assert price_for_double_eco(25) == 47.52
-    assert price_for_double_eco(23) == 47.52
+def test_calcul_price_max_price_set():
+    rate1 = Rate(n_rooms=11, n_room_increase=3, min_price=40, increase=None, max_price=69)  # ~19,93%
+    rate2 = Rate(n_rooms=12, n_room_increase=4, min_price=40, increase=None, max_price=69)  # ~31.33%
+    # rate.increase = math.pow(rate.max_price/rate.min_price, 1 / (rate.n_rooms // rate.n_room_increase)) - 1
 
-    assert price_for_double_eco(22) - 51.31 <= 0.1
-    assert price_for_double_eco(20) - 51.31 <= 0.1
+    assert calcul_price(11, None, rate1, 0) == 40
+    assert calcul_price(10, None, rate1, 0) == 40
+    assert calcul_price(9, None, rate1, 0) == 40
+    assert calcul_price(8, None, rate1, 0) == 47
+    assert calcul_price(6, None, rate1, 0) == 47
+    assert calcul_price(5, None, rate1, 0) == 57
+    assert calcul_price(3, None, rate1, 0) == 57
+    assert calcul_price(2, None, rate1, 0) == 69
+    assert calcul_price(1, None, rate1, 0) == 69
 
-    assert price_for_double_eco(3) - 81.43 <= 0.1
-    assert price_for_double_eco(2) - 81.43 <= 0.1
+    assert calcul_price(12, None, rate2, 0) == 40
+    assert calcul_price(11, None, rate2, 0) == 40
+    assert calcul_price(9, None, rate2, 0) == 40
 
-    assert price_for_double_eco(1) - 87.95 <= 0.1
+    assert calcul_price(8, None, rate2, 0) == 52
+    assert calcul_price(5, None, rate2, 0) == 52
 
-    assert price_for_double_eco(0) == 100
-    assert price_for_double_eco(-1) == 100
+    assert calcul_price(4, None, rate2, 0) == 69
+    assert calcul_price(1, None, rate2, 0) == 69
 
-    
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "graph":
-            graph_price()
-        else:
-            print(price_for_double_eco(int(sys.argv[1])))
+    pass
