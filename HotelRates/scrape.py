1c10cd55506024bf47c2a59301921411feea0f9d
XuanShine <xuan.ng@hotmail.com>
2021-01-22 08:51:41 +0100

    update link booking

--- HotelRates/scrape.py
+++ HotelRates/scrape.py
@@ -44,8 +44,11 @@ def scrape(url, retry=0):
 
 
 
-URL_TEMPLATE = "https://www.booking.com/searchresults.en-gb.html?label=gen173nr-1DCAEoggI46AdIM1gEaKQCiAEBmAEJuAEHyAEM2AED6AEBiAIBqAIDuALCxJ31BcACAQ&lang=en-gb&sid=4c66111699f2737e59fa9bb34705aef5&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.en-gb.html%3Flabel%3Dgen173nr-1DCAEoggI46AdIM1gEaKQCiAEBmAEJuAEHyAEM2AED6AEBiAIBqAIDuALCxJ31BcACAQ%3Bsid%3D4c66111699f2737e59fa9bb34705aef5%3Bsb_price_type%3Dtotal%26%3B&ss=Grasse%2C+Provence-Alpes-C%C3%B4te+d%27Azur%2C+France&is_ski_area=&ssne=Cannes&ssne_untouched=Cannes&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1&ss_raw=grasse&ac_position=0&ac_langcode=en&ac_click_type=b&dest_id=-1430518&dest_type=city&place_id_lat=43.659992&place_id_lon=6.92362&search_pageview_id=5f0d92c3aeac038d&search_selected=true&search_pageview_id=5f0d92c3aeac038d&ac_suggestion_list_length=5&ac_suggestion_theme_list_length=0"
+URL_TEMPLATE = "https://www.booking.com/searchresults.fr.html?aid=202160&sid=4c66111699f2737e59fa9bb34705aef5&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.fr.html%3Faid%3D202160%3Bsid%3D4c66111699f2737e59fa9bb34705aef5%3Bsb_price_type%3Dtotal%26%3B&ss=Grasse&is_ski_area=0&ssne=Grasse&ssne_untouched=Grasse&dest_id=-1430518&dest_type=city&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1"
+# "https://www.booking.com/searchresults.en-gb.html?label=gen173nr-1DCAEoggI46AdIM1gEaKQCiAEBmAEJuAEHyAEM2AED6AEBiAIBqAIDuALCxJ31BcACAQ&lang=en-gb&sid=4c66111699f2737e59fa9bb34705aef5&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.en-gb.html%3Flabel%3Dgen173nr-1DCAEoggI46AdIM1gEaKQCiAEBmAEJuAEHyAEM2AED6AEBiAIBqAIDuALCxJ31BcACAQ%3Bsid%3D4c66111699f2737e59fa9bb34705aef5%3Bsb_price_type%3Dtotal%26%3B&ss=Grasse%2C+Provence-Alpes-C%C3%B4te+d%27Azur%2C+France&is_ski_area=&ssne=Cannes&ssne_untouched=Cannes&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1&ss_raw=grasse&ac_position=0&ac_langcode=en&ac_click_type=b&dest_id=-1430518&dest_type=city&place_id_lat=43.659992&place_id_lon=6.92362&search_pageview_id=5f0d92c3aeac038d&search_selected=true&search_pageview_id=5f0d92c3aeac038d&ac_suggestion_list_length=5&ac_suggestion_theme_list_length=0"
 URL_TEMPLATE2 = "https://www.booking.com/searchresults.fr.html?aid=318615&label=French_France-FR-FR-116605733887-4eIPfuV0xgYOMDFPkQsxiAS479723944020%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi146292847245%3Atiaud-297601666955%3Adsa-1073823970767%3Alp9054940%3Ali%3Adec%3Adm&sid=4c66111699f2737e59fa9bb34705aef5&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.fr.html%3Faid%3D318615%3Blabel%3DFrench_France-FR-FR-116605733887-4eIPfuV0xgYOMDFPkQsxiAS479723944020%253Apl%253Ata%253Ap1%253Ap2%253Aac%253Aap%253Aneg%253Afi146292847245%253Atiaud-297601666955%253Adsa-1073823970767%253Alp9054940%253Ali%253Adec%253Adm%3Bsid%3D4c66111699f2737e59fa9bb34705aef5%3Bsb_price_type%3Dtotal%3Bsrpvid%3D3346a73e0b0000d6%26%3B&ss=Mouans-Sartoux&is_ski_area=0&ssne=Mouans-Sartoux&ssne_untouched=Mouans-Sartoux&dest_id=-1453848&dest_type=city&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1"
+
+
 def buildUrlBooking(datefrom, dateto=None, ibis=False):
     """Build a url booking of list of hotel near Provence Alpes Cotes d’Azur.
     datefrom and dateto: dd/mm/aaaa
@@ -108,4 +111,4 @@ def main(days=6*30):
         writeInFile(result_temp)
 
 if __name__ == "__main__":
-    main(30*3)
+    main(5)
